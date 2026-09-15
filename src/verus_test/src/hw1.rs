3445145853cae92661a212cfa1eec1c10f43d02b
samanthastaheli <sstaheli27@gmail.com>
2026-09-14 19:26:46 -0600

    problem 1 works

--- src/verus_test/src/hw1.rs
+++ src/verus_test/src/hw1.rs
@@ -5,7 +5,7 @@ verus! {
 spec fn spec_is_in_range_by_step(value: int, min: int, max: int, step: nat) -> (result: bool) {
     if min <= value <= max {
         let diff = value - min;
-        diff % step == 0
+        diff % (step as int) == 0
     } else {
         false
     }
@@ -14,6 +14,7 @@ spec fn spec_is_in_range_by_step(value: int, min: int, max: int, step: nat) -> (
 exec fn is_in_range_by_step(value: i32, min: i32, max: i32, step: u16) -> (result: bool)
     requires
         step > 0,
+        max - min <= i32::MAX as int, // proves won't overflow
     ensures
         result == spec_is_in_range_by_step(value as int, min as int, max as int, step as nat),
 {
@@ -21,8 +22,8 @@ exec fn is_in_range_by_step(value: i32, min: i32, max: i32, step: u16) -> (resul
         return false;
     }
     let diff = value - min;
-    let remainder = diff.checked_rem_euclid(step);
-    if remainder == 0 {
+    let remainder = diff.checked_rem_euclid(step as i32);
+    if remainder == Some(0) {
         true
     } else {
         false
@@ -30,8 +31,12 @@ exec fn is_in_range_by_step(value: i32, min: i32, max: i32, step: u16) -> (resul
 }
 
 fn main() {
-    assert(is_in_range_by_step(10, 0, 20, 5) == true);
-    assert(is_in_range_by_step(11, 0, 20, 5) == false);
+
+    // Problem 1
+    let result1 = is_in_range_by_step(10, 0, 20, 5);
+    assert(result1 == true);
+    let result2 = is_in_range_by_step(11, 0, 20, 5);
+    assert(result2 == false);
 }
 
 } // verus!
