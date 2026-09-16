2b078d902a6eab47ef11f760e0de74d731c985d0
Samantha Staheli <sstaheli27@gmail.com>
2026-09-16 12:00:38 -0600

    moved verus test folder

--- verus_test/src/main.rs
+++ verus_test/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use vstd::prelude::*;
+
+verus! {
+
+spec fn min(x: int, y: int) -> int {
+    if x <= y {
+        x
+    } else {
+        y
+    }
 }
+
+fn main() {
+    assert(min(10, 20) == 10);
+    assert(min(-10, -20) == -20);
+    assert(forall|i: int, j: int| min(i, j) <= i && min(i, j) <= j);
+    assert(forall|i: int, j: int| min(i, j) == i || min(i, j) == j);
+    assert(forall|i: int, j: int| min(i, j) == min(j, i));
+    assert(forall|i: int, j: int| min(i, j) == min(i, i));
+}
+
+} // verus!
